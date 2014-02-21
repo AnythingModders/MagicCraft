6e17449c14f54e8bf666e25616d7f386f7e05b58
AnythingModders <bwfcwalshy@gmail.com>
2014-02-21 11:25:39 +0000

    ClientProxy base class

---
+++ com/bwfcwalshy/magicCraft/Client/ClientProxy.java
@@ -0,0 +1,13 @@
+package com.bwfcwalshy.MagicCraft.client;
+
+import net.minecraftforge.client.MinecraftForgeClient;
+import com.bwfcwalshy.MagicCraft.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+      
+       @Override
+       public void registerRenderers() {
+               // This is for rendering entities and so forth later on
+       }
+      
+}
