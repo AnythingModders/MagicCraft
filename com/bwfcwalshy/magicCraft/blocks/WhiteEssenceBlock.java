5ad3b942cc6cc0219709684ba4522020c22f4e43
AnythingModders <bwfcwalshy@gmail.com>
2014-02-21 11:28:49 +0000

    White Essence block (Building Block)

---
+++ com/bwfcwalshy/magicCraft/blocks/WhiteEssenceBlock.java
@@ -0,0 +1,11 @@
+package com.bwfcwalshy.MagicCraft.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class WhiteEssenceBlock extends Block{
+
+	public WhiteEssenceBlock(int id, Material material){
+		super(id, material);
+	}
+}
