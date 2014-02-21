0aa6df7777a10e809ee69c20d0c5888f085b79eb
AnythingModders <bwfcwalshy@gmail.com>
2014-02-21 11:26:46 +0000

    A simple reference file

---
+++ com/bwfcwalshy/magicCraft/BasicInfo.java
@@ -0,0 +1,8 @@
+package com.bwfcwalshy.MagicCraft;
+
+public class BasicInfo {
+
+	public static final String ID = "MagicCraft";
+	public static final String NAME = "MagicCraft";
+	public static final String VERS = "0.0.5";
+}
