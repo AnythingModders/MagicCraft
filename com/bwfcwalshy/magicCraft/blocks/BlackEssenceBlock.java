f890f60320a359e016fc2243aea6e41d6677dd74
AnythingModders <bwfcwalshy@gmail.com>
2014-02-21 11:28:02 +0000

    Black Essence Block (Building Block)

---
+++ com/bwfcwalshy/magicCraft/blocks/BlackEssenceBlock.java
@@ -0,0 +1,11 @@
+package com.bwfcwalshy.MagicCraft.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlackEssenceBlock extends Block {
+
+	public BlackEssenceBlock(int id, Material material) {
+		super(id, material);
+	}
+}
